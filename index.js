1ce4bde7b0354e557b3aa731495f50b8883248ab
Palakkasaudhan <Palakkasaudhan123@gamil.com>
2024-11-29 21:49:51 +0530

    palindrome is added

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 // start coding here
-let arr =[1,2,3,4,5]
+let arr = [1, 2, 3, 4, 5]
 
-let  count =0;
-for(let i=0; i<arr.length; i++){
-    if(arr[i]%2==1){
+let count = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 1) {
         count++;
     }
 }
 console.log(`count of odd numbers is $(count)`)
+
+
+let str = "racecar"
+let bag = ""
+
+for (let i = str.length - 1; i >= 0; i--) {
+    bag = bag + str[i]
+}
+if (bag == str) {
+    return true;
+} else {
+    return false;
+}
